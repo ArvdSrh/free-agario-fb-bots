033c4345c41d4159dbb9d6949770a55307018d52
Genius <56919066+GeniusXD@users.noreply.github.com>
2019-11-09 00:22:22 +0800

    Updated to latest :)

--- userscript/free-agario-bots.hslo.user.js
+++ userscript/free-agario-bots.hslo.user.js
@@ -1,47 +1,48 @@
 // ==UserScript==
-// @name         HSLO BOTS SCRIPT
-// @description  HSLO WITH AGAR BOTS
-// @version      1.0.10
-// @author       2CL & test1145
+// @name         HSLO + NelBots
+// @description  HSLO
+// @version      7.1
+// @author       test114514
 // @match        *://agar.io/*
 // @require      http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js
 // @run-at       document-start
 // @grant        none
 // ==/UserScript==
+
 if (location.host === 'agar.io' && location.href !== 'https://agar.io/hslo') {
-    location.href = 'https://agar.io/hslo';
-    return;
+  location.href = 'https://agar.io/hslo';
+  return;
 }
 
 const HSLO = new class {
-    constructor() {
-        this.method = 'GET';
-        this.URL = 'https://saigo.hslo.io/';
-        this.HTML = ``;
-        this.date = Date.now();
-    }
+  constructor() {
+    this.method = 'GET';
+    this.URL = 'https://saigo.hslo.io/';
+    this.HTML = ``;
+    this.date = Date.now();
+  }
 
-    load() {
-        this.setMessage();
-        this.fetch();
-    }
+  load() {
+    this.setMessage();
+    this.fetch();
+  }
 
-    setMessage() {
-        document.body.innerHTML = "LOADING... PLEASE WAIT..";
-    }
+  setMessage() {
+    document.body.innerHTML = "LOADING...";
+  }
 
-    fetch() {
-        const request = new XMLHttpRequest();
-        request.open(this.method, this.URL, true);
-        request.onload = () => {
-            this.HTML = request.responseText;
-            this.write();
-        };
-        request.onerror = () => {
-            document.body.innerHTML = "<div style='width: 100%; text-align: center; font-size: 24px; font-family: sans-serif;'>Failed to fetch HSLO files.</div>";
-        }
-        request.send();
+  fetch() {
+    const request = new XMLHttpRequest();
+    request.open(this.method, this.URL, true);
+    request.onload = () => {
+      this.HTML = request.responseText;
+      this.write();
+    };
+    request.onerror = () => {
+      document.body.innerHTML = "<div style='width: 100%; text-align: center; font-size: 24px; font-family: sans-serif;'>Failed to fetch HSLO files.</div>";
     }
+    request.send();
+  }
     replace(hello) {
         return hello.replace(/<script\s+src="https\:\/\/saigo\.hslo\.io\/saigo\.js?.*"><\/script>/, `
 <script src="http://ex-script.com/fstyle/hslo/nelsc.js?d=${this.date}"></script>
@@ -61,11 +62,11 @@ loadUI()
 `);
     }
 
-    write() {
-        document.open();
-        document.write(this.replace(this.HTML));
-        document.close();
-    }
+  write() {
+    document.open();
+    document.write(this.replace(this.HTML));
+    document.close();
+  }
 }
 
 HSLO.load();
